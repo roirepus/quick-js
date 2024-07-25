2a409b785923a50d84df8616fa55f82e32ef84e4
roirepus <rajsinsinwar159@gmail.com>
2024-07-25 13:47:39 +0530

    started

---
+++ number-and-math.js
@@ -0,0 +1,38 @@
+// //2a
+// const soupPrice=10;
+// const burgerPrice=8;
+// const icecreamPrice=5;
+//
+// var qtySoup=1;
+// var qtyBurger=3;
+// var qtyIcecream=1;
+//
+// var total=(soupPrice*qtySoup)+(burgerPrice*qtyBurger)+(icecreamPrice*qtyIcecream);
+// console.log(total);
+//
+// //2b
+// const qtyOrder=3;
+// total=total*qtyOrder;
+// console.log(total);
+//
+// //2c
+// //2d
+// const toasterPrice=18.50;
+// const shirtPrice=7.50;
+//
+// var qtyToaster=1;
+// var qtyShirt=2;
+//
+// var total=((1850*1)+(750*2))/100;
+// const taxPercent=20;//10
+// const taxAmount=total*(taxPercent)/100;
+// console.log(total);
+// console.log(taxAmount);
+//
+// //2j
+// console.log(Math.floor(2.8));
+// //2k
+// console.log(Math.ceil(2.2));
+
+const unit=prompt("select unit of temp(c/f): ");
+console.log("your choice", unit);
---
+++ strings.js
@@ -0,0 +1,7 @@
+// console.log(typeof(typeof 'hello'));
+// console.log('a'+'b');
+// console.log(('q'+3)); //type coercion
+// console.log(typeof('q'+3));
+const a=4;
+const b=4;
+console.log(`$${(a+b)}`);
