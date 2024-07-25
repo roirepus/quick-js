809bc6c858e565f855de87c3cae1a55253a67c47
roirepus <rajsinsinwar159@gmail.com>
2024-07-25 17:08:38 +0530

    refreshed basics

---
+++ objects.js
@@ -0,0 +1,11 @@
+const product ={
+  name: 'socks',
+price: 1090,
+	
+};
+product.name=5;
+console.log(product.name);
+product.color='red';
+console.log(product.color);
+delete product.price;
+console.log(product)
---
+++ varibles.js
@@ -0,0 +1,5 @@
+let var1=3;
+ console.log(var1);
+ let calc=2+2;
+console.log(calc);
+
